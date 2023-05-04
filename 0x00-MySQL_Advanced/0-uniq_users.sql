ab37b17024a3165b283f057c9d2ad3d670b80701
david onwuli <dcazicruz@icloud.com>
2023-05-04 18:03:17 +0100

    task0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
--- a SQL script that creates a table users
---attr: id (int), name (string), email (string)
+-- SQL script that creates a table users
+-- attr: id (int), name (string), email (string)
 CREATE TABLE IF NOT EXISTS users (
         id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
         email varchar(255) NOT NULL UNIQUE,

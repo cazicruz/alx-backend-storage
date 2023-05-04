89eb8be0b036fa5d4af5ac69aa5a9efb18306718
david onwuli <dcazicruz@icloud.com>
2023-05-04 17:53:42 +0100

    task0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,8 +2,8 @@
 --attr: id (int), name (string), email (string)
 
 CREATE TABLE users IF NOT EXISTS (
-    id INT NOT NULL AUTO_INCREMENT,
-    name CHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    id int NOT NULL AUTO_INCREMENT,
+    name char(255),
+    email varchar(255) NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );

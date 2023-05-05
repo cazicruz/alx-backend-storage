28b36cc71028941ae0ac799505b9f273ca90cce0
david onwuli <dcazicruz@icloud.com>
2023-05-05 10:48:04 +0100

    task 0

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,4 @@
 -- sql cript to import database
-
-mysql -uroot -p metal_bands 
-
 -- a SQL script that ranks country origins of bands,
 -- ordered by the number of (non-unique) fans
 SELECT RANK() OVER(ORDER BY COUNT(*) DESC) AS

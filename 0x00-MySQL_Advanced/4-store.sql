e3413d437f32a86ee0c6c4b19a0498eda65c0306
david onwuli <dcazicruz@icloud.com>
2023-05-08 08:24:58 +0100

    task 0

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -5,6 +5,6 @@ TRIGGER item_order_trigger AFTER INSERT
 ON orders
 FOR EACH ROW BEGIN
 	UPDATE items SET quantity = quantity - NEW.number
-	WHERE name = NEW.item_name
+	WHERE name = NEW.item_name;
 END$$
 DELIMITER ; $$

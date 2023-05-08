bdaff3775c292fb750601daba4451cc837e5ea7e
david onwuli <dcazicruz@icloud.com>
2023-05-08 08:27:47 +0100

    task 0

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,6 @@
--- triger that reduces the number of items when a new order is added
-DELIMITER $$ ;
-CREATE
-TRIGGER item_order_trigger AFTER INSERT
+-- triger that reduces the number of items when a new order is adde
+CREATE TRIGGER item_order_trigger AFTER INSERT
 ON orders
-FOR EACH ROW BEGIN
+FOR EACH ROW
 	UPDATE items SET quantity = quantity - NEW.number
 	WHERE name = NEW.item_name;
-END$$
-DELIMITER ; $$

659729ad96620e9c799c83a1744e5d77c3738878
david onwuli <dcazicruz@icloud.com>
2023-05-10 08:35:53 +0100

    task 0

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Python function that changes all topics of
+ a school document based on the name"""
+
+
+ def update_topics(mongo_collection, name, topics):
+     """ 
+     name : string
+     topic : list[string]
+     mongo_collection : pymongo obj
+     """
+     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})

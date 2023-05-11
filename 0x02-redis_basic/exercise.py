3d6b32d5b811e149ab9e82c6f436b559d463593a
david onwuli <dcazicruz@icloud.com>
2023-05-11 16:28:51 +0100

    task 100

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 import redis, uuid
 
+
 class Cache():
+    """a class called cache"""
 
     def __init__():
+        """init method of the class stores an instance of redis"""
         _redis = redis.Redis()
         _redis.flushdb
 
     def store(data) -> str:
+        """stores data in Redis using the a random value
+        as key and returns the key"""
         key = uuid.uuid4()
         _redis.set({key: data})
         return key
